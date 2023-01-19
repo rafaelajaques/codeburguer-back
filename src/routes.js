997232f552db5082e2b68f44f274e0876000aa0c
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-01-19 16:51:24 -0300

    Migração de usuário criada

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
+import {v4} from 'uuid'
+import User from './app/models/User'
+
 const routes = new Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+routes.get("/", async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Rafaela',
+    email: 'rafaela@email.com',
+    password_hash:'hse45h8',
+    admin: false,
+  })
+  return response.json(user);
 });
 
 export default routes;
