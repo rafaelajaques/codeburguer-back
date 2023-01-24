91f5d0fdbdd287a9bf54cec78b900087fed19b72
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-01-24 12:55:56 -0300

    .

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,19 +1,42 @@
 import {v4} from 'uuid'
 import User from '../models/User'
+import * as Yup from 'yup'
 
 class UserController{
     async store (request, response){
-        const { name, email, password_hash, admin} = request.body
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().required().email(),
+            password: Yup.string().required().min(6),
+            admin: Yup.boolean()
+        })
+
+        try{
+            await schema.validateSync(request.body, {abortEarly: false})
+        } catch(err){
+            return response.status(400).json({error: err.errors})
+        }
+        
+        const { name, email, password, admin} = request.body
+
+        const userExits = await User.findOne({
+            where: {email},
+        })
+
+        if(userExits){
+            return response.status(400).json({error: 'User already exists'})
+        }
 
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin,
         })
 
-        return response.json(user)
+        return response.status(201).json({id: user.id, name, email, admin})
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = 3003
+const port = 3004
 app.listen(port, () =>{
     console.log(`Server started on port ${port}`)
 });
