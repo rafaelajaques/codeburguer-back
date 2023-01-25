e5b51a0788b91bbc5d28eca43ce09c9ae55cf7f1
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-01-25 17:36:48 -0300

    Tabela de categoria

--- src/database/migrations/20230125190415-create-categories.js
+++ src/database/migrations/20230125190415-create-categories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('products', {
+    await queryInterface.createTable('categories', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -12,18 +12,7 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
         allowNull: false,
-      },
-      price: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      category: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      path: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        unique: true,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -33,11 +22,11 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-    })
+    });
+
   },
 
   async down(queryInterface) {
-    await queryInterface.dropTable('products');
-
+    await queryInterface.dropTable('categories');
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import ProductController from './app/controllers/ProductController'
+import CategoryController from "./app/controllers/CategoryController";
 import multer from "multer";
 import multerConfig from './config/multer'
 import authMiddleware from './app/middlewares/auth'
@@ -14,8 +15,12 @@ routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
 
 routes.use(authMiddleware)
+
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
+
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 
 export default routes;
