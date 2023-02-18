536bda21c66d289731a71e5ce087bb469e00b0ea
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-02-18 18:15:35 -0300

    Categorias e produtos cadastrados

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,101 +1,106 @@
-import * as Yup from 'yup'
-import Product from '../models/Product'
-import Category from '../models/Category'
-import User from '../models/User'
+import * as Yup from "yup";
+import Product from "../models/Product";
+import Category from "../models/Category";
+import User from "../models/User";
 class ProductController {
-    async store(request, response) {
-        const schema = Yup.object().shape({
-            name: Yup.string().required(),
-            price: Yup.number().required(),
-            category_id: Yup.number().required(),
-            offer: Yup.boolean()
-        })
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      category_id: Yup.number().required(),
+      offer: Yup.boolean(),
+    });
 
-        try {
-            await schema.validateSync(request.body, { abortEarly: false })
-        } catch (err) {
-            return response.status(400).json({ error: err.errors })
-        }
-
-        const { admin: isAdmin } = await User.findByPk(request.userId)
-
-        if (!isAdmin) {
-            return response.status(401).json()
-        }
-
-        const { filename: path } = request.file
-        const { name, price, category_id, offer } = request.body
-        const product = await Product.create({
-            name,
-            price,
-            category_id,
-            path,
-            offer
-        })
-
-        return response.json(product)
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
     }
 
-    async index(request, response) {
-        const products = await Product.findAll({
-            include: [
-                {
-                    model: Category,
-                    as: 'category',
-                    attributes: ['id', 'name']
-                }
-            ]
-        })
-        return response.json(products)
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json();
     }
 
-    async update(request, response) {
-        const schema = Yup.object().shape({
-            name: Yup.string(),
-            price: Yup.number(),
-            category_id: Yup.number(),
-            offer: Yup.boolean()
-        })
+    const { filename: path } = request.file;
+    const { name, price, category_id, offer } = request.body;
+    const product = await Product.create({
+      name,
+      price,
+      category_id,
+      path,
+      offer,
+    });
 
-        try {
-            await schema.validateSync(request.body, { abortEarly: false })
-        } catch (err) {
-            return response.status(400).json({ error: err.errors })
-        }
+    return response.json(product);
+  }
 
-        const { admin: isAdmin } = await User.findByPk(request.userId)
-
-        if (!isAdmin) {
-            return response.status(401).json()
-        }
-
-        const { id } = request.params
-        const product = await Product.findByPk(id)
-
-        if (!product) {
-            return response.status(401).json({ error: "Make sure  your product ID is correct" })
-        }
-
-        let path
-        if (request.file) {
-            path = request.file.filename
-        }
-
-        const { name, price, category_id } = request.body
-
-        await Product.update({
-            name,
-            price,
-            category_id,
-            path,
+  async index(request, response) {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: "category",
+          attributes: ["id", "name"],
         },
-            { where: { id } }
-        )
+      ],
+    });
+    return response.json(products);
+  }
 
-        return response.status(200).json()
-    }catch(err){
-        console.log(err)
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
+    });
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
     }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json();
+    }
+
+    const { id } = request.params;
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return response
+        .status(401)
+        .json({ error: "Make sure  your product ID is correct" });
+    }
+
+    let path;
+    if (request.file) {
+      path = request.file.filename;
+    }
+
+    const { name, price, category_id, offer } = request.body;
+
+    await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+      },
+      { where: { id } }
+    );
+
+    return response.status(200).json();
+  }
+  catch(err) {
+    console.log(err);
+  }
 }
 
-export default new ProductController()
+export default new ProductController();
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,29 +1,31 @@
-import Sequelize, { Model } from 'sequelize'
+import Sequelize, { Model } from "sequelize";
 
 class Product extends Model {
-    static init(sequelize) {
-        super.init({
-            name: Sequelize.STRING,
-            price: Sequelize.INTEGER,
-            path: Sequelize.STRING,
-            url: {
-                type: Sequelize.VIRTUAL,
-                get() {
-                    return `http://localhost:3004/product-file/${this.path}`
-                }
-            }
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        price: Sequelize.INTEGER,
+        path: Sequelize.STRING,
+        offer: Sequelize.BOOLEAN,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3004/product-file/${this.path}`;
+          },
         },
-            { sequelize }
-        )
-        return this
-    }
+      },
+      { sequelize }
+    );
+    return this;
+  }
 
-    static associate(models) {
-        this.belongsTo(models.Category, {
-            foreignKey: 'category_id',
-            as: 'category'
-        })
-    }
+  static associate(models) {
+    this.belongsTo(models.Category, {
+      foreignKey: "category_id",
+      as: "category",
+    });
+  }
 }
 
-export default Product
+export default Product;
