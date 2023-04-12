ff535223c7c76f28a147e6454bd5321ba04dab26
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-04-12 20:23:40 -0300

    Mensagem do servidor atualizada

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = 3004
-app.listen(port, () =>{
-    console.log(`Server started on port ${port}`)
+const port = 3004;
+app.listen(port, () => {
+  console.log(`Server started on port ${port} 🚀`);
 });
